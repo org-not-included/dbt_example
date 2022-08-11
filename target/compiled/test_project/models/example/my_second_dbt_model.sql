12b178640099c89a3c11fdb0da4ae9035c8f817f
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-08-11 14:52:06 -0500

    Sample PR (#1)
    * added initial readme
    
    * fix main.yml
    
    * set DBT_PROFILE_FILE
    
    * set keyfile location to location expected by github action
    
    * trying different vars for org and repo name
    
    * Added additional step, to ensure all our vars are set properly

--- target/compiled/test_project/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from `ultimate-bit-359101`.`prod_example`.`my_first_dbt_model`
-where id = 1
--- target/compiled/test_project/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
+++
@@ -1,9 +0,0 @@
-
-    
-    
-
-select *
-from `ultimate-bit-359101`.`prod_None`.`my_second_dbt_model`
-where id is null
-
-
